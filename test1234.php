d5321e865f8e3a92cf84a655dfa123cf08c255a1
Maciej Palmowski <m.palmowski@freshpixels.pl>
2022-01-27 00:37:53 +0100

    first commit

---
+++ test1234.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Test1234
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     test1234
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Test1234
+ */
+
+// Your code starts here.
